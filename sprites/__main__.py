940caa81b8a42c1a65614ddda3491643d00042bd
Joshua Conrad <jozbornn@gmail.com>
2022-04-02 20:03:18 -0700

    Palette shifting test
    No bugs!

--- sprites/__main__.py
+++ sprites/__main__.py
@@ -1,6 +1,7 @@
 from sprites import *
 
 import pygame
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((640, 640))
@@ -8,29 +9,69 @@ screen = pygame.display.set_mode((640, 640))
 player = Player()
 player.change_state("fall")
 
+scrolls = Scroll(5, 7, palette="nes"), Scroll(4, 3)
+vines = Vines(3), Vines(7, palette="nes")
+bangs = Bang(), Bang(5, 3, palette="nes")
+ink = Ink(level=2)
+pencils = Pencil(), Pencil("red", palette="nes")
+statue = Statue("horns1")
+dust = Dust()
+
+
 def sample_draw():
-    # Scrolls
     screen.fill((0, 0, 0))
-    screen.blit(Scroll(5, 7, palette="nes").surface, (100, 100))
-    screen.blit(Scroll(4, 3, palette="pico-8").surface, (200, 250))
+
+    # Scrolls
+    for s in scrolls:
+        if random.randint(0, 10) == 0:
+            s.shift_palette()
+    screen.blit(scrolls[0].surface, (100, 100))
+    screen.blit(scrolls[1].surface, (200, 250))
 
     # Vines
-    screen.blit(Vines(3).surface, (400, 400))
-    screen.blit(Vines(7, palette="nes").surface, (150, 220))
+    for v in vines:
+        if random.randint(0, 10) == 0:
+            v.shift_palette()
+    screen.blit(vines[0].surface, (400, 400))
+    screen.blit(vines[1].surface, (150, 220))
 
     # Bangs
-    screen.blit(Bang().surface, (500, 0))
-    screen.blit(Bang(5, 3, palette="nes").surface, (0, 100))
+    for b in bangs:
+        if random.randint(0, 10) == 0:
+            b.shift_palette()
+    screen.blit(bangs[0].surface, (500, 0))
+    screen.blit(bangs[1].surface, (0, 100))
 
     # Ink
-    screen.blit(Ink(level=2).surface, (400, 200))
+    if random.randint(0, 10) == 0:
+        ink.shift_palette()
+    if random.randint(0, 10) == 0:
+        ink.set_level((ink.level - 1) % 7)
+    if random.randint(0, 10) == 0:
+        ink.change_color()
+    screen.blit(ink.surface, (400, 200))
 
     # Pencil
-    screen.blit(Pencil().surface, (400, 100))
-    screen.blit(Pencil("red", palette="nes").surface, (250, 150))
+    for p in pencils:
+        if random.randint(0, 10) == 0:
+            p.shift_palette()
+        if random.randint(0, 10) == 0:
+            p.change_color()
+    screen.blit(pencils[0].surface, (400, 100))
+    screen.blit(pencils[1].surface, (250, 150))
 
     # Statue
-    screen.blit(Statue("eye2").surface, (500, 500))
+    if random.randint(0, 10) == 0:
+        statue.shift_palette()
+    screen.blit(statue.surface, (500, 500))
+
+    # Dust
+    screen.blit(dust.surface, (500, 300))
+    dust.tick()
+
+    # Player
+    screen.blit(player.surface, (500, 200))
+    player.tick()
 
 
 run = True
@@ -41,8 +82,5 @@ while run:
         elif event.type == pygame.KEYDOWN:
             pass
     sample_draw()
-
-    screen.blit(player.surface, (500, 200))
-    player.tick()
     pygame.display.update()
     pygame.time.wait(100)
