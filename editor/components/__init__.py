d115663559bc05666c5aff3c5e35662c460e5cff
Joshua Conrad <jozbornn@gmail.com>
2023-05-17 06:44:30 -0700

    Create chunks

--- editor/components/__init__.py
+++ editor/components/__init__.py
@@ -1,3 +1,4 @@
 from editor.components.camera import Camera
+from editor.components.chunk import Chunk
 from editor.components.cursor import Cursor
 from editor.components.player import Player
---
+++ editor/components/chunk.py
@@ -0,0 +1,10 @@
+
+
+class Chunk:
+    def __init__(self):
+        self.tiles = [
+            [] for _ in range(14)
+        ]
+        self.collectibles = {
+
+        }
